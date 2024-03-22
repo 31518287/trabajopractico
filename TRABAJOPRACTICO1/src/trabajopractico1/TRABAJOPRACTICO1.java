a657783595af9caceccbbaf21b0eda50ef780bb0
SergioAmieva <163580270+SergioAmieva@users.noreply.github.com>
2024-03-22 17:28:40 -0300

    Update TRABAJOPRACTICO1.java

--- TRABAJOPRACTICO1/src/trabajopractico1/TRABAJOPRACTICO1.java
+++ TRABAJOPRACTICO1/src/trabajopractico1/TRABAJOPRACTICO1.java
@@ -9,6 +9,7 @@ public class TRABAJOPRACTICO1 {
         System.out.println("hola mundo william xzdxdxdxdxdxd");
         System.out.println("william y rodrigo");
         System.out.println("Ramiro");
+        System.out.println("Sergio");
     }
     
 }
