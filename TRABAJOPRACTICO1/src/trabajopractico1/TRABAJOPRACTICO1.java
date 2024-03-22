66e06e4d10be0735dee002e7d57a7c9ec5fe5df8
31518287 <williamsebastianbrito@hotmail.com>
2024-03-22 16:49:24 -0300

    equipo
    asasasasasasasasa

---
+++ TRABAJOPRACTICO1/src/trabajopractico1/TRABAJOPRACTICO1.java
@@ -0,0 +1,13 @@
+
+package trabajopractico1;
+
+public class TRABAJOPRACTICO1 {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("hola mundo william xzdxdxdxdxdxd");
+      
+    }
+    
+}
